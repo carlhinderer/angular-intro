05149987d30a15ec630eefdb6410987cd49b8aec
Carl Hinderer <carl.hinderer4@gmail.com>
2020-01-15 20:08:45 -0700

    Added fields and default values for stock item component.

--- projects/stock-market/src/app/stock/stock-item/stock-item.component.ts
+++ projects/stock-market/src/app/stock/stock-item/stock-item.component.ts
@@ -5,11 +5,19 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './stock-item.component.html',
   styleUrls: ['./stock-item.component.css']
 })
+
 export class StockItemComponent implements OnInit {
+  public name: string;
+  public code: string;
+  public price: number;
+  public previousPrice: number;
 
   constructor() { }
 
   ngOnInit() {
+    this.name = 'Test Stock Company';
+    this.code = 'TSC';
+    this.price = 85;
+    this.previousPrice = 80;
   }
-
 }
